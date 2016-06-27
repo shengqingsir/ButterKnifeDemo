2d1ca983d07e755f4bcfe00a5c6f730d86f48c6c
sheng <461360332@qq.com>
2016-06-27 19:38:01 +0800

    礼物说——ButterKnife!

--- app/src/main/java/com/android/shengqing/butterknifedemo/MainActivity.java
+++ app/src/main/java/com/android/shengqing/butterknifedemo/MainActivity.java
@@ -3,8 +3,8 @@ package com.android.shengqing.butterknifedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
